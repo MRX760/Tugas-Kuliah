cb032f2624c6a1fd675dc266b64aab25734d962f
Sultan Ahmad Alfath <59986264+MRX760@users.noreply.github.com>
2022-12-11 13:57:07 +0700

    Add files via upload

---
+++ Tugas Pemweb/Praktikum/Tugas Praktikum 5/index.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data </title>
+</head>
+<body>
+
+<form id="form_mahasiswa">
+<td>Prodi</td>
+<select name="prodi" id="prodi">
+    <option value="">Pilih Program Studi</option>
+<option value="Informatika">Teknik Informatika</option>
+<option value="Kelautan">Teknik Kelautan</option>
+</select>
+</form>
+<br>
+
+<div id="tampil_data">
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>NAMA</th>
+            <th>Program Studi</th>
+        </tr>
+</table>
+
+<script src = "https://code.jquery.com/jquery-3.6.1.min.js"
+integrity = "sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+crossorigin = "anonymous">
+</script>
+<script>
+$(document).ready(function(){
+    $("#prodi").change(function(){
+        var value=$(this).val();
+    $.ajax({
+        url:"tampil.php",
+        type:"POST",
+        data:"prodi=" + value,
+        success:function(data){
+            $("#tampil_data").html(data);
+        }
+    }) 
+});
+});
+</script>
+</head>
+</body>
+</html>
